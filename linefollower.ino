68b15c0b6d2e467819ee950ed6410ffd667facc7
Abhinav Vats <abhinavvats08@gmail.com>
2026-03-07 20:07:54 +0530

    Fix LinkedIn URL in comments

--- linefollower.ino
+++ linefollower.ino
@@ -1,7 +1,7 @@
 // ===== 8 SENSOR PID LINE FOLLOWER =====
 // Arduino Nano + TB6612FNG + 8 Analog IR Sensors
 //ABHINAV VATS
-//www.lnkedin.com/in/abhinavvats06
+//www.linkedin.com/in/abhinavvats06
 
 // -------- IR Sensors --------
 int sensorPins[8] = {A0,A1,A2,A3,A4,A5,A6,A7};
